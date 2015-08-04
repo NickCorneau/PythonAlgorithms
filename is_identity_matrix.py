db577741178e004bda696f4ffb11b7ad7d5f4de9
Nicholas Corneau <nicholascorneau@gmail.com>
2015-08-04 14:50:44 -0400

    returns whether or not matrix is identity
    # Given a list of lists representing a n * n matrix as input, 
    # define a  procedure that returns True if the input is an identity matrix 
    # and False otherwise.
    
    # An IDENTITY matrix is a square matrix in which all the elements 
    # on the principal/main diagonal are 1 and all the elements outside 
    # the principal diagonal are 0. 
    # (A square matrix is a matrix in which the number of rows 
    # is equal to the number of columns)

--- is_identity_matrix.py
+++ is_identity_matrix.py
@@ -1,14 +1,3 @@
-# By Ashwath from forums
-# Given a list of lists representing a n * n matrix as input, 
-# define a  procedure that returns True if the input is an identity matrix 
-# and False otherwise.
-
-# An IDENTITY matrix is a square matrix in which all the elements 
-# on the principal/main diagonal are 1 and all the elements outside 
-# the principal diagonal are 0. 
-# (A square matrix is a matrix in which the number of rows 
-# is equal to the number of columns)
-
 def is_identity_matrix(matrix):
     if (len(matrix) != len(matrix[0])):
         return False
