46714b8266f0bd1ece32d21dac680b10de7cd904
Nicholas Corneau <nicholascorneau@gmail.com>
2015-08-04 14:52:37 -0400

    determines whether or not matrix is antisymmetric
    # Modify Problem Set 31's (Optional) Symmetric Square to return True 
    # if the given square is antisymmetric and False otherwise. 
    # An nxn square is called antisymmetric if A[i][j]=-A[j][i] 
    # for each i=0,1,...,n-1 and for each j=0,1,...,n-1.

--- antisymmetric.py
+++ antisymmetric.py
@@ -1,8 +1,3 @@
-# Modify Problem Set 31's (Optional) Symmetric Square to return True 
-# if the given square is antisymmetric and False otherwise. 
-# An nxn square is called antisymmetric if A[i][j]=-A[j][i] 
-# for each i=0,1,...,n-1 and for each j=0,1,...,n-1.
-
 def antisymmetric(A):
     #Write your code here
     for row in range(len(A)):
